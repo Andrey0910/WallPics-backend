319f3e7ce5c4e6de21a41913eb0398afd8bd6a5b
Andrey Petrov <petrovaa71@mail.ru>
2022-12-29 23:59:54 +0300

    добавил admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,8 +24,8 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware('role:admin')->prefix('admin_panel')->group(function () {
-//    Route::get('/', [HomeController::class, 'index'])->name('homeAdmin');
-//
+    Route::get('/', [HomeController::class, 'index'])->name('homeAdmin');
+
 //    Route::resource('/category', CategoryController::class);
 //    Route::resource('/post', PostController::class);
 });
